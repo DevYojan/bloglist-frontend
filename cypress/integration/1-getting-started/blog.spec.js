42b3173bbf57164b8952739c87e29a6bfdbbc055
Yojan Regmi <devyojan@gmail.com>
2021-08-08 12:49:18 +0400

    testing login form

--- cypress/integration/1-getting-started/blog.spec.js
+++ cypress/integration/1-getting-started/blog.spec.js
@@ -1,6 +1,11 @@
 describe('blog app', () => {
   beforeEach(() => {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      username: 'yojan',
+      password: 'regmi',
+      name: 'Yojan Regmi',
+    });
     cy.visit('http://localhost:3000');
   });
 
@@ -9,5 +14,21 @@ describe('blog app', () => {
     cy.contains('Username');
     cy.contains('Password');
     cy.get('button').contains('Login');
+  });
+
+  describe('Login', () => {
+    it('is successful with correct credentials', () => {
+      cy.get('#username').type('yojan');
+      cy.get('#password').type('regmi');
+      cy.get('button').click();
+      cy.contains('yojan logged in !');
+    });
+
+    it('fails with wrong credentials', () => {
+      cy.get('#username').type('yalien');
+      cy.get('#password').type('bhatij');
+      cy.get('button').click();
+      cy.contains('yojan logged in !').should('not.exist');
+    });
   });
 });
